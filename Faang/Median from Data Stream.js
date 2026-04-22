bcedfdeb6a9970910a21a59dd17ce0d7438bc67b
Nawin chander <nawinnapzy289@gmail.com>
2026-04-22 20:55:12 +0530

    add

--- Faang/Median from Data Stream.js
+++ Faang/Median from Data Stream.js
@@ -10,3 +10,37 @@
 // Keeps median balanced
 
 
+class MedianFinder {
+  constructor() {
+    this.small = new MaxHeap();
+    this.large = new MinHeap();
+  }
+
+  addNum(num) {
+    this.small.insert(num);
+
+    if (this.small.peek() > this.large.peek()) {
+      this.large.insert(this.small.extractMax());
+    }
+
+    if (this.small.size() > this.large.size() + 1) {
+      this.large.insert(this.small.extractMax());
+    }
+
+    if (this.large.size() > this.small.size()) {
+      this.small.insert(this.large.extractMin());
+    }
+  }
+
+  findMedian() {
+    if (this.small.size() > this.large.size()) {
+      return this.small.peek();
+    }
+
+    return (this.small.peek() + this.large.peek()) / 2;
+  }
+}
+
+
+//// This demonstrates mastery of balancing heaps.
+
