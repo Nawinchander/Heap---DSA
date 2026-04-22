be93212067753051922b4795f6da7f06af172540
Nawin chander <nawinnapzy289@gmail.com>
2026-04-22 20:53:29 +0530

    add

---
+++ Faang/Merge K Sorted Lists.js
@@ -0,0 +1,30 @@
+// Merge K Sorted Lists
+
+// Very common at Google.
+
+// Use heap to always extract smallest among K lists.
+
+
+function mergeKSortedArrays(arrays) {
+  const heap = new MinHeap();
+  const result = [];
+
+  for (let i = 0; i < arrays.length; i++) {
+    if (arrays[i].length > 0) {
+      heap.insert([arrays[i][0], i, 0]);
+    }
+  }
+
+  while (heap.size()) {
+    let [value, arrIdx, elIdx] = heap.extractMin();
+    result.push(value);
+
+    if (elIdx + 1 < arrays[arrIdx].length) {
+      heap.insert([arrays[arrIdx][elIdx + 1], arrIdx, elIdx + 1]);
+    }
+  }
+
+  return result;
+}
+
+
