5aff86d931345ed64282b3753fa5968227440fbb
Nawin chander <nawinnapzy289@gmail.com>
2026-04-23 19:55:36 +0530

    add

---
+++ Faang/IPO Problem.js
@@ -0,0 +1,17 @@
+// Problem: Choose at most k projects to maximize capital.
+
+// Each project has:
+
+// required capital
+// profit
+
+// Heap Pattern:
+// Sort by capital
+// Max Heap for profits
+
+
+// Interview Thinking:  “Among affordable projects, pick most profitable.
+
+
+
+
