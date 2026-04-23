e7fb9b2a356a64d1dd93d59a40dd67c0b4b0402a
Nawin chander <nawinnapzy289@gmail.com>
2026-04-23 19:48:08 +0530

    add

--- Faang/Meeting Rooms II.js
+++ Faang/Meeting Rooms II.js
@@ -5,4 +5,26 @@
 // Heap Pattern: Use Min Heap for meeting end times.
 
 
+// Interview Thinking: “Reuse earliest ending room if available.”
+
+
+function minMeetingRooms(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  const heap = [];
+
+  for (let [start, end] of intervals) {
+    heap.sort((a, b) => a - b);
+
+    if (heap.length && heap[0] <= start) {
+      heap.shift();
+    }
+
+    heap.push(end);
+  }
+
+  return heap.length;
+}
+
+
 
