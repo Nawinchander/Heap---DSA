0d0af35559141fffc89effb38b5e0f665558673b
Nawin chander <nawinnapzy289@gmail.com>
2026-04-23 19:53:11 +0530

    add

---
+++ Faang/Smallest Range Covering.js
@@ -0,0 +1,14 @@
+// Smallest Range Covering Elements from K Lists
+
+// This is a real Google-style heap problem.
+
+// Problem: Given K sorted lists, find the smallest range that includes at least one number from each list.
+
+// Heap Pattern: Use Min Heap, track current maximum.
+
+// Interview Thinking: “Keep one element from each list and shrink range.”
+
+
+
+
+
