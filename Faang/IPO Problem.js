2379a2425ccb89891dfcef867ce27c870889d86f
Nawin chander <nawinnapzy289@gmail.com>
2026-04-23 19:55:57 +0530

    add

--- Faang/IPO Problem.js
+++ Faang/IPO Problem.js
@@ -13,5 +13,27 @@
 // Interview Thinking:  “Among affordable projects, pick most profitable.
 
 
+function findMaxCapital(k, w, profits, capital) {
+  let projects = profits.map((p, i) => [capital[i], p]);
+  projects.sort((a, b) => a[0] - b[0]);
+
+  let heap = [];
+  let i = 0;
+
+  for (let j = 0; j < k; j++) {
+    while (i < projects.length && projects[i][0] <= w) {
+      heap.push(projects[i][1]);
+      i++;
+    }
+
+    if (!heap.length) break;
+
+    heap.sort((a, b) => b - a);
+    w += heap.shift();
+  }
+
+  return w;
+}
+
 
 
