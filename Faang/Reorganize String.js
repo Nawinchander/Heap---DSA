29bf2b6f12643c7aa818d45fbc5a8a1333b2ad2f
Nawin chander <nawinnapzy289@gmail.com>
2026-04-23 19:45:58 +0530

    add

--- Faang/Reorganize String.js
+++ Faang/Reorganize String.js
@@ -6,3 +6,40 @@
 // Output: "ababac"
 
 
+// Heap Pattern:
+
+// Use Max Heap
+
+// Why?
+
+// Always pick the character with highest remaining frequency.
+
+// Interview Thinking:
+
+// “Greedily place the most frequent char, but prevent repeating previous char.”
+
+
+function reorganizeString(s) {
+  const freq = {};
+  for (let ch of s) freq[ch] = (freq[ch] || 0) + 1;
+
+  let heap = Object.entries(freq).sort((a, b) => b[1] - a[1]);
+  let prev = null, result = "";
+
+  while (heap.length) {
+    heap.sort((a, b) => b[1] - a[1]);
+    let [char, count] = heap.shift();
+
+    result += char;
+    count--;
+
+    if (prev) heap.push(prev);
+    prev = count > 0 ? [char, count] : null;
+  }
+
+  return result.length === s.length ? result : "";
+}
+
+
+
+
