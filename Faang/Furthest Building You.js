1adc5703013c9f1085999e7bb2fed491afd09e50
Nawin chander <nawinnapzy289@gmail.com>
2026-04-23 19:50:13 +0530

    add

---
+++ Faang/Furthest Building You.js
@@ -0,0 +1,16 @@
+// Problem: You have bricks and ladders.
+
+// Find the furthest building reachable.
+
+// Heap Pattern: Use Min Heap
+
+// Store climbs where ladders were used.
+
+// Interview Thinking: “Use ladders for biggest climbs, bricks for smaller climbs.”
+
+// Heap helps dynamically decide.
+
+
+
+
+
